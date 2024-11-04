07486ac7e674457218bdfcc282ba1fe652e24b7a
KobeLimon21 <kobelimon2121@gmail.com>
2024-11-04 14:00:14 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,9 +1,9 @@
-function tfib(n, a = 0, b = 1){
-if (n == 0){
- return a;   
+function tfib(n, a = 0, b = 1){ 
+if (n == 0){ 
+ return a; // base case of 0 
 }
 if ( n == 1 ){
- return b; 
+ return b; // base case of 1 
 }
 return tfib(n-1,b,a+b)
 }
